86994742a2779d0e38c8fdccd18f933ee3ab9106
らくえもん <85974181+RaKuemonn@users.noreply.github.com>
2022-05-01 16:39:29 +0900

    調整中

--- Assets/Scripts/System/FloorDataTable.cs
+++ Assets/Scripts/System/FloorDataTable.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
@@ -17,6 +18,7 @@ public class FloorData  // 10m*10mマスの床が持つデータ
 {
     public int id = 0;                              // 床の識別子 (手動割り振り)
     public FloorInfoMoveSpeed move_speed_state = FloorInfoMoveSpeed.Stop; // 床が持っている速度状態 (この状態をプレイヤーが当たり判定を用いて受け取ることで、プレイヤーの速度を制御させている)
+    public StopFloorConditionExpr FloorConditionExpr;   // Stop床から走り出す条件式
 }
 public enum FloorInfoMoveSpeed
 {
@@ -25,3 +27,49 @@ public enum FloorInfoMoveSpeed
     Run,        // 最大速度の床
     SpeedDown,  // 速度を落とし始める床
 }
+
+//[CustomEditor(typeof(FloorData))]
+//public class FloorDataEditor : Editor
+//{
+//    //private FloorData _target;
+
+//    private SerializedProperty _property;
+
+//    private void Awake()
+//    {
+//        _property = serializedObject.FindProperty("FloorData");
+//    }
+
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        EditorGUILayout.PropertyField(_property);
+//        serializedObject.ApplyModifiedProperties();
+
+//        //EditorGUI.BeginChangeCheck();
+
+//        //_target.id = EditorGUILayout.IntField("ID", _target.id);
+
+//        //_target.move_speed_state =
+//        //    (FloorInfoMoveSpeed)EditorGUILayout.EnumPopup("FloorInfoMoveSpeed", _target.move_speed_state);
+
+
+//        //if (_target.move_speed_state == FloorInfoMoveSpeed.Stop)
+//        //{
+//        //    EditorGUILayout.LabelField("Cond Expr for Run");
+//        //    _target.FloorConditionExpr = 
+//        //        (StopFloorConditionExpr)EditorGUILayout.ObjectField(
+//        //            "FloorConditionExpr Component has GameObject",
+//        //            _target.FloorConditionExpr,
+//        //            typeof(StopFloorConditionExpr), true);
+
+//        //}
+
+//        //// GUIの更新があったら実行
+//        //if (EditorGUI.EndChangeCheck())
+//        //{
+//        //    EditorUtility.SetDirty(_target);
+//        //}
+//    }
+//}
+
--- Assets/Scripts/System/StopFloorCondExpr.cs
+++ Assets/Scripts/System/StopFloorCondExpr.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(FloorInfo))]
 public class StopFloorConditionExpr : MonoBehaviour
 {
-    
+    [SerializeField] private Func<bool> ConditionExpression;
+
+    // °‚ÌğŒ®‚ğƒNƒŠƒA‚µ‚½ (true or false), —§‚¿~‚Ü‚é°‚©‚çÄ‚Ñ‘–‚èn‚ß‚é‚½‚ß‚ÌğŒ® 
+    public bool OnCompleteFloorCondExpr()
+    {
+        return ConditionExpression.Invoke();
+    }
 }
