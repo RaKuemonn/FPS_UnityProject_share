94a29c52a0205f8d17e0cc184d7070c6c4acb824
Naruse27 <88305459+Naruse27@users.noreply.github.com>
2022-05-27 11:53:26 +0900

    unko
    unp

--- Assets/Scripts/EnemyBossController.cs
+++ Assets/Scripts/EnemyBossController.cs
@@ -233,20 +233,19 @@ public class EnemyBossController : MonoBehaviour
         {
             if (rotateCube.GetRotateCheck())
             {
-                ConditionSickleAttackBerserkerState();
-                //if (hp < hpMax / 2)
-                //{
-                //    int test = Random.Range(0, 3);
-                //    if (test == 0) ConditionSickleAttackState();
-                //    if (test == 1) ConditionSickleAttackBerserkerState();
-                //    if (test == 2) ConditionAssaultAttackState();
-                //}
-                //else
-                //{
-                //    int test = Random.Range(0, 2);
-                //    if (test == 0) ConditionSickleAttackState();
-                //    if (test == 1) ConditionAssaultAttackState();
-                //}
+                if (hp < hpMax / 2)
+                {
+                    int test = Random.Range(0, 3);
+                    if (test == 0) ConditionSickleAttackState();
+                    if (test == 1) ConditionSickleAttackBerserkerState();
+                    if (test == 2) ConditionAssaultAttackState();
+                }
+                else
+                {
+                    int test = Random.Range(0, 2);
+                    if (test == 0) ConditionSickleAttackState();
+                    if (test == 1) ConditionAssaultAttackState();
+                }
             }
         }
 
