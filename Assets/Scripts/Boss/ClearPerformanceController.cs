50c753433bbec96cb0f9897ff9798544bd6db3cc
らくえもん <85974181+RaKuemonn@users.noreply.github.com>
2022-05-31 18:33:08 +0900

    バグりそう

--- Assets/Scripts/Boss/ClearPerformanceController.cs
+++ Assets/Scripts/Boss/ClearPerformanceController.cs
@@ -20,10 +20,12 @@ public class ClearPerformanceController : MonoBehaviour
         if (boss.GetDeathFlag() == false) return;
         if (boss.GetEndDeathAnimation() == false) return;
 
+        Time.timeScale = 0.0f;
         ClearUIs.SetActive(true); 
         
         // Aƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚Ä‚¢‚½‚ç
         if (Button_A.triggered == false) return;
+        Time.timeScale = 1.0f;
         GetComponent<ChangeScene>().Change();
     }
 }
